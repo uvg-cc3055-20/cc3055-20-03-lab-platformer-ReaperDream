d83777877eb2acbbe715f3f5e9fb0372716b4f1a
ReaperDream <henryt.o@hotmail.com>
2018-01-31 22:44:39 -0600

    Terminado

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -11,12 +11,14 @@ public class Character : MonoBehaviour {
     private float speed = 5f;
     private float jumpForce = 250f;
     private bool facingRight = true;
+    Animator anim;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+        anim = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -27,7 +29,8 @@ public class Character : MonoBehaviour {
             cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
             facingRight = move > 0;
         }
-        
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
         sr.flipX = !facingRight;
 
         if (Input.GetButtonDown("Jump")) {
